9d21191c441bba918fe0cdc03c0efd1b6269aba3
Zoldan <denz060289@gmail.com>
2022-09-17 15:05:54 +0300

    end_program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] startingArray = new string[] {"hello", "2", "world", ":-)"};
+string[] resultArray = new string [0];
+
+foreach (var item in startingArray)
+{
+    if (item.Length <= 3)
+    {
+    Array.Resize(ref resultArray, resultArray.Length + 1);
+    resultArray [resultArray.Length-1] = item;
+    }
+}
+
+Console.WriteLine($"[{String.Join("; ", resultArray)}]");
